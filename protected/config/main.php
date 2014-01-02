b3c9fa775740cdfb8455e3e0b5e5ea47a813add8
Robert Einsle <robert@einsle.de>
2014-01-02 07:59:00 +0100

    config for urls in pathinfo

--- protected/config/main.php
+++ protected/config/main.php
@@ -37,6 +37,7 @@ return array(
         // uncomment the following to enable URLs in path-format
         'urlManager' => array(
             'urlFormat' => 'path',
+            'showScriptName'=>false,
             'rules' => array(
                 // REST patterns
                 array('api/list', 'pattern' => 'api/<model:\w+>', 'verb' => 'GET'),
